7cccb31b9ffe05b0683eefa07158f44455e3ce91
Felipe Pessina <fs.pessina@gmail.com>
2020-08-20 18:16:47 -0300

    add express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
